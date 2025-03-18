c93662fc5e1f5f5a84dedf99f140566b6aa35ab6
Luis Fernando <luisfernando.mourag@gmail.com>
2025-03-17 21:49:38 -0300

    Fazendo a Estilização do Layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
-import { Header } from '../components/Header';
-
 export function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Header />
         </div>
-
     )
 }
