297f24a5284bedd9c3aa86b521c6a09becdfb959
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-19 14:23:23 -0300

    Validando Formulários

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import { Play } from 'phosphor-react';
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60),
+})
+
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
 
     function handleCreateNewCicle(data: any) {
         // TODO: Create new cicle with the form data
