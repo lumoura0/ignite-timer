56f68024560587afc939b63fe4d3f83bb1a8e7cd
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-19 14:09:36 -0300

    Adicionando a biblioteca react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,28 @@
 import { Play } from 'phosphor-react';
+import { useForm } from 'react-hook-form'
 import { HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from './styles';
 
 export function Home() {
+    const { register, handleSubmit, watch } = useForm()
+
+    function handleCreateNewCicle(data: any) {
+        // TODO: Create new cicle with the form data
+        console.log(data)
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
                         placeholder='Dê um nome para o seu projeto'
                         list='task-suggestions'
+                        {...register('task')}
                     />
 
                     <datalist id='task-suggestions'>
@@ -25,6 +37,7 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
                     <span>minutos.</span>
@@ -38,7 +51,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled type='submit'>
+                <StartCountdownButton disabled={isSubmitDisabled} type='submit'>
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
