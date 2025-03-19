87c41b543669e428316aac658fb5555d304011c2
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-19 12:58:00 -0300

    Estilizando Componente History

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-import { History } from './pages/History'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
 
 export function Router() {
     return (
